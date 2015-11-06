b5cbaca12f0b4a4a8cd18c81e35eaf271e6ab236
Sai Ram Nellutla <ramnellutla1@gmail.com>
2015-11-06 16:42:05 -0500

    Update IndoorNavigationActivity.java

--- app/src/main/java/com/example/u2/raksha/IndoorNavigationActivity.java
+++ app/src/main/java/com/example/u2/raksha/IndoorNavigationActivity.java
@@ -6,8 +6,11 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -17,20 +20,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
-
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -39,150 +35,114 @@ import java.util.List;
 public class IndoorNavigationActivity extends AppCompatActivity {
 
     private BluetoothAdapter mBluetoothAdapter;
-    private BluetoothLeScanner mBluetoothLeScanner = BluetoothAdapter.getDefaultAdapter().getBluetoothLeScanner();
     private boolean mScanning;
     private Handler mHandler;
-    private LeDeviceListAdapter mLeDeviceListAdapter;
-
+    String status;
+    BluetoothLeScanner mLEScanner;
+    private List<ScanFilter> filters;
+    private ScanSettings settings;
+    private int REQUEST_ENABLE_BT = 1;
     // Stops scanning after 10 seconds.
     private static final long SCAN_PERIOD = 10000;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         ParseUser user;
-        String status;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_indoor_navigation);
         user = ParseUser.getCurrentUser();
         status = (String) user.get("status");
-        mHandler = new Handler();
         pullMap();
-        discoverDevice();
-
-    }
-
-    private void discoverDevice() {
-
-        // Use this check to determine whether BLE is supported on the device. Then
-        // you can selectively disable BLE-related features.
+        mHandler = new Handler();
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "BLE Not Supported",
+                    Toast.LENGTH_SHORT).show();
             finish();
         }
-
-        // Initializes Bluetooth adapter.
         final BluetoothManager bluetoothManager =
                 (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         mBluetoothAdapter = bluetoothManager.getAdapter();
 
-        // Ensures Bluetooth is available on the device and it is enabled. If not,
-        // displays a dialog requesting user permission to enable Bluetooth.
-        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
-            mBluetoothAdapter.enable();
-        }
-        // Checks if Bluetooth is supported on the device.
-        if (mBluetoothAdapter == null) {
-            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
-            finish();
-            return;
-        }
-        scanLeDevice(true);
     }
 
+
     private void scanLeDevice(final boolean enable) {
+
         if (enable) {
-            // Stops scanning after a pre-defined scan period.
             mHandler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    mScanning = false;
-                    mBluetoothLeScanner.stopScan(mLeScanCallback);
-                    invalidateOptionsMenu();
+                    if (Build.VERSION.SDK_INT < 21) {
+                        mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                    } else {
+                        mLEScanner.stopScan(mScanCallback);
+
+                    }
                 }
             }, SCAN_PERIOD);
-            mScanning = true;
-            mBluetoothLeScanner.startScan(mLeScanCallback);
-        } else {
-            mScanning = false;
-            mBluetoothLeScanner.stopScan(mLeScanCallback);
-        }
-        invalidateOptionsMenu();
-    }
-    // Adapter for holding devices found through scanning.
-    private class LeDeviceListAdapter extends BaseAdapter {
-        private ArrayList<BluetoothDevice> mLeDevices;
-        private LayoutInflater mInflator;
-        public LeDeviceListAdapter() {
-            super();
-            mLeDevices = new ArrayList<BluetoothDevice>();
-            mInflator = IndoorNavigationActivity.this.getLayoutInflater();
-        }
-        public void addDevice(BluetoothDevice device) {
-            if(!mLeDevices.contains(device)) {
-                mLeDevices.add(device);
-            }
-        }
-        public BluetoothDevice getDevice(int position) {
-            return mLeDevices.get(position);
-        }
-        public void clear() {
-            mLeDevices.clear();
-        }
-        @Override
-        public int getCount() {
-            return mLeDevices.size();
-        }
-        @Override
-        public Object getItem(int i) {
-            return mLeDevices.get(i);
-        }
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-      @Override
-       public View getView(int i, View view, ViewGroup viewGroup) {
-/*            RecyclerView.ViewHolder viewHolder;
-            // General ListView optimization code.
-            if (view == null) {
-                view = mInflator.inflate(R.layout.listitem_device, null);
-                viewHolder = new RecyclerView.ViewHolder();
-                viewHolder.deviceAddress = (TextView) view.findViewById(R.id.device_address);
-                viewHolder.deviceName = (TextView) view.findViewById(R.id.device_name);
-                view.setTag(viewHolder);
+            if (Build.VERSION.SDK_INT < 21) {
+                mBluetoothAdapter.startLeScan(mLeScanCallback);
             } else {
-                viewHolder = (RecyclerView.ViewHolder) view.getTag();
+                mLEScanner.startScan(filters, settings, mScanCallback);
             }
-            BluetoothDevice device = mLeDevices.get(i);
-            final String deviceName = device.getName();
-            if (deviceName != null && deviceName.length() > 0)
-                viewHolder.deviceName.setText(deviceName);
-            else
-                viewHolder.deviceName.setText(R.string.unknown_device);
-            viewHolder.deviceAddress.setText(device.getAddress());
-            */
-          return view;
+        } else {
+            if (Build.VERSION.SDK_INT < 21) {
+                mBluetoothAdapter.stopLeScan(mLeScanCallback);
+            } else {
+                mLEScanner.stopScan(mScanCallback);
+            }
         }
+
     }
     // Device scan callback.
-    private ScanCallback mLeScanCallback =
-            new ScanCallback() {
+    private ScanCallback mScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            Log.i("callbackType", String.valueOf(callbackType));
+            Log.i("result", result.toString());
+            BluetoothDevice btDevice = result.getDevice();
+            if(btDevice.toString().equalsIgnoreCase("64:5A:04:56:E3:18")){
+                Toast.makeText(IndoorNavigationActivity.this.getApplication(),btDevice.toString()+"I m here",Toast.LENGTH_SHORT).show();
+            }
+
+
+/*            if(btDevice.getName() != null ){
+                Toast.makeText(IndoorNavigationActivity.this.getApplication(),btDevice.getName().toString()+"I m here",Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(IndoorNavigationActivity.this.getApplication(),btDevice.toString()+"I m here",Toast.LENGTH_SHORT).show();
+            }*/
+
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            for (ScanResult sr : results) {
+                Log.i("ScanResult - Results", sr.toString());
+            }
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            Log.e("Scan Failed", "Error Code: " + errorCode);
+        }
+    };
+
+    private BluetoothAdapter.LeScanCallback mLeScanCallback =
+            new BluetoothAdapter.LeScanCallback() {
                 @Override
-                public void onScanResult(int callbackType, final ScanResult result) {
+                public void onLeScan(final BluetoothDevice device, int rssi,
+                                     byte[] scanRecord) {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            String name = result.getDevice().getName();
-                            Toast.makeText(IndoorNavigationActivity.this.getApplicationContext(),name,Toast.LENGTH_SHORT).show();
+                            Log.i("onLeScan", device.toString());
                         }
                     });
                 }
             };
 
-    static class ViewHolder {
-        TextView deviceName;
-        TextView deviceAddress;
-    }
+
     private void pullMap() {
         ParseQuery<ParseObject> query = ParseQuery.getQuery("IndoorRegisters");
         query.whereEqualTo("reg", "1");
@@ -194,7 +154,7 @@ public class IndoorNavigationActivity extends AppCompatActivity {
                             .execute(plan.getUrl());
                 } else {
                     Log.d("score", "Error: " + e.getMessage());
-                    Toast.makeText(IndoorNavigationActivity.this, "Connection to Database failed. Check your Internet.", Toast.LENGTH_LONG)
+                    Toast.makeText(IndoorNavigationActivity.this, "Connection to Database failed. Check your Internet.", Toast.LENGTH_SHORT)
                             .show();
                 }
             }
@@ -222,6 +182,31 @@ public class IndoorNavigationActivity extends AppCompatActivity {
 
         protected void onPostExecute(Bitmap result) {
             bmImage.setImageBitmap(result);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        } else {
+            if (Build.VERSION.SDK_INT >= 21) {
+                mLEScanner = mBluetoothAdapter.getBluetoothLeScanner();
+                settings = new ScanSettings.Builder()
+                        .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                        .build();
+                filters = new ArrayList<ScanFilter>();
+            }
+            scanLeDevice(true);
+        }
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mBluetoothAdapter != null && mBluetoothAdapter.isEnabled()) {
+            //scanLeDevice(false);
         }
     }
 }
