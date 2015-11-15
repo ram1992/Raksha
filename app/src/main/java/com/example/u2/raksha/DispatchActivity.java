7d27f9afa3b13b10651172dcf94137216a01917d
Sai Ram Nellutla <ramnellutla1@gmail.com>
2015-11-14 22:37:38 -0500

    Update

--- app/src/main/java/com/example/u2/raksha/DispatchActivity.java
+++ app/src/main/java/com/example/u2/raksha/DispatchActivity.java
@@ -7,6 +7,7 @@ package com.example.u2.raksha;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.parse.ParseUser;
 
@@ -17,25 +18,40 @@ import java.io.Serializable;
  * (SignUpOrLoginActivity) activity.
  */
 public class DispatchActivity extends Activity {
-
-    public DispatchActivity() {
-    }
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dispatch);
         // Check if there is current user info
         if (ParseUser.getCurrentUser() != null) {
             // Start an intent for the logged in activity
+            handler.postDelayed(new Runnable() {
 
-            Intent intent = new Intent(DispatchActivity.this, MainActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-            //intent.putExtra("currentUser", ParseUser.getCurrentUser());
-            startActivity(intent);
-        } else {
-            // Start and intent for the logged out activity
-            startActivity(new Intent(this, WelcomeActivity.class));
+                public void run() {
+                    Intent intent = new Intent(DispatchActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    //intent.putExtra("currentUser", ParseUser.getCurrentUser());
+                    startActivity(intent);
+                }
+            }
+                ,3000);
+            }else {
+            handler.postDelayed(new Runnable() {
+
+                public void run() {
+                    // Start and intent for the logged out activity
+                    startActivity(new Intent(DispatchActivity.this, WelcomeActivity.class));
+
+                }
+            }, 1000);
         }
+    }
+
+    @Override
+    public void onResume() {  // After a pause OR at startup
+        super.onResume();
     }
 
 }
